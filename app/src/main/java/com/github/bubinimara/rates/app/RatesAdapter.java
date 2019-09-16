823c9ec8033887b3df83c8c0e37e18e3a0914dfd
Davide <bubini.mara5@gmail.com>
2019-09-16 16:58:15 +0200

    show mock data

---
+++ app/src/main/java/com/github/bubinimara/rates/app/RatesAdapter.java
@@ -0,0 +1,71 @@
+package com.github.bubinimara.rates.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.bubinimara.rates.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by davide.
+ */
+public class RatesAdapter extends RecyclerView.Adapter<RatesAdapter.Holder> {
+
+    private LayoutInflater layoutInflater;
+    private final @NonNull  List<RateModel> rates;
+
+    public RatesAdapter(@NonNull Context context) {
+        layoutInflater = LayoutInflater.from(context);
+        rates = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.rates_list_item, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.set(getItem(position));
+    }
+
+    protected RateModel getItem(int position){
+        return rates.get(position);
+    }
+    @Override
+    public int getItemCount() {
+        return rates.size();
+    }
+
+    public void updateRates(@NonNull List<RateModel> rateModels) {
+        rates.clear();
+        rates.addAll(rateModels);
+    }
+
+    public static class Holder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_code)
+        TextView code;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+
+        void set(RateModel rate){
+            code.setText(rate.getCode());
+        }
+    }
+}
--- app/src/main/java/com/github/bubinimara/rates/app/RatesFragment.java
+++ app/src/main/java/com/github/bubinimara/rates/app/RatesFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,12 +16,28 @@ import android.view.ViewGroup;
 
 import com.github.bubinimara.rates.R;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RatesFragment extends Fragment {
 
     private RatesViewModel mViewModel;
 
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    RatesAdapter adapter;
+
     public static RatesFragment newInstance() {
         return new RatesFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new RatesAdapter(getContext().getApplicationContext());
     }
 
     @Override
@@ -32,7 +50,20 @@ public class RatesFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(RatesViewModel.class);
-        // TODO: Use the ViewModel
+        mViewModel.getRatesLiveData().observe(this,this::onDataChanged);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void onDataChanged(List<RateModel> rateModels) {
+        adapter.updateRates(rateModels);
     }
 
 }
