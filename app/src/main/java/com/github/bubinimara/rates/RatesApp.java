547138e7eaa09882d68e32c5814b1e8752ade06e
Davide <bubini.mara5@gmail.com>
2019-09-16 14:42:51 +0200

    initial commit

---
+++ app/src/main/java/com/github/bubinimara/rates/RatesApp.java
@@ -0,0 +1,9 @@
+package com.github.bubinimara.rates;
+
+import android.app.Application;
+
+/**
+ * Created by davide.
+ */
+public class RatesApp extends Application {
+}
---
+++ app/src/main/java/com/github/bubinimara/rates/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.bubinimara.rates.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.bubinimara.rates.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
