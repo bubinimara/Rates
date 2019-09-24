8b14651897d0e634c4c5db1452835188caeb80a5
Davide <bubini.mara5@gmail.com>
2019-09-24 15:23:22 +0200

    calculate the value into domain

--- app/src/main/java/com/github/bubinimara/rates/domain/RatesInteractor.java
+++ app/src/main/java/com/github/bubinimara/rates/domain/RatesInteractor.java
@@ -8,26 +8,25 @@ import com.github.bubinimara.rates.domain.repo.ExchangeRate;
 import com.github.bubinimara.rates.domain.repo.Repository;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Flowable;
-import io.reactivex.Observable;
-import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by davide.
  * like GetRatesUseCase and calculate
+ * Note: A good approach will be to separate the task to fetch and the task to compute the value in two different classes
+ *
+ * todo: add option ( in repository ) to not update when change only the value, like get from cache and then update
+ *
  */
 public class RatesInteractor {
     private final static long TIMER_PERIOD = 1;
 
-    private MutableLiveData<List<ExchangeRate>> rates;
+    private MutableLiveData<List<Rate>> rates;
     private final Repository repository;
     private Disposable disposable;
 
@@ -36,9 +35,9 @@ public class RatesInteractor {
         this.repository = RepositoryImpl.createMockRepository();
     }
 
-    public void fetchRatesAtFixedTime(final String code){
+    public void fetchRatesAtFixedTime(final String code,final double value){
         clear();
-        fetchRates(code);
+        fetchRates(code,value);
     }
 
     public void clear(){
@@ -48,11 +47,13 @@ public class RatesInteractor {
     }
     /**
      * built in cold observable and backpressure
-     * @param code
+     * @param code the currency code
+     * @param value the currency value
      */
-    protected void fetchRates(String code){
+    protected void fetchRates(String code,double value){
         disposable = Flowable.interval(0, TIMER_PERIOD, TimeUnit.SECONDS)
                 .flatMap(t -> repository.getExchangeRate(code).toFlowable())
+                .map(exchangeRates -> createRate(exchangeRates,value))
                 .subscribeOn(Schedulers.io())
                 .subscribe(exchangeRates -> {
                     rates.postValue(exchangeRates);
@@ -62,7 +63,22 @@ public class RatesInteractor {
 
     }
 
-    public LiveData<List<ExchangeRate>> getRatesLiveData(){
+    /**
+     * Calculate the value and map the result
+     * Should use Big*
+     * @param exchangeRates
+     * @param value
+     * @return
+     */
+    private List<Rate> createRate(List<ExchangeRate> exchangeRates,double value) {
+        List<Rate> result = new ArrayList<>(exchangeRates.size());
+        for (ExchangeRate er : exchangeRates) {
+            result.add(new Rate(er.getCode(),er.getDescription(),er.getExchangeRate()*value,er.getIconUrl()));
+        }
+        return result;
+    }
+
+    public LiveData<List<Rate>> getRatesLiveData(){
         return rates;
     }
 }
