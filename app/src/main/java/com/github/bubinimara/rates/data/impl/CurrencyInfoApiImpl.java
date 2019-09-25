f263e3f8a663233aec416afcaec7fba86b536fd1
Davide <bubini.mara5@gmail.com>
2019-09-25 09:15:36 +0200

    get currency info

---
+++ app/src/main/java/com/github/bubinimara/rates/data/impl/CurrencyInfoApiImpl.java
@@ -0,0 +1,34 @@
+package com.github.bubinimara.rates.data.impl;
+
+import com.github.bubinimara.rates.data.CurrencyInfoApi;
+import com.github.bubinimara.rates.data.model.CurrencyInfoEntity;
+
+import java.util.Currency;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by davide.
+ */
+class CurrencyInfoApiImpl implements CurrencyInfoApi {
+
+    private static final String BASE_IMAGE_URL = "https://raw.githubusercontent.com/transferwise/currency-flags/master/src/flags/";
+
+    @Override
+    public Observable<CurrencyInfoEntity> getCurrencyInfo(String code) {
+        CurrencyInfoEntity result = new CurrencyInfoEntity(code,getCurrencyDescription(code),getCurrencyImageUrl(code));
+        return Observable.just(result);
+    }
+
+    /**
+     * transferwise user have a collection of flags in his repo in github, not good quality
+     * https://github.com/transferwise/currency-flags
+     */
+    private String getCurrencyImageUrl(String code) {
+        return BASE_IMAGE_URL+code+".png";
+    }
+
+    private String getCurrencyDescription(String code) {
+        return Currency.getInstance(code).getDisplayName();
+    }
+}
