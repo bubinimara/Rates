314596b226fcc271915fe1037b8d61a9cea09145
Davide <bubini.mara5@gmail.com>
2019-09-26 14:41:10 +0200

    di - expose ui module

--- app/src/main/java/com/github/bubinimara/rates/RatesApp.java
+++ app/src/main/java/com/github/bubinimara/rates/RatesApp.java
@@ -2,8 +2,36 @@ package com.github.bubinimara.rates;
 
 import android.app.Application;
 
+import com.github.bubinimara.rates.di.component.ApplicationComponent;
+import com.github.bubinimara.rates.di.component.DaggerApplicationComponent;
+import com.github.bubinimara.rates.di.component.DaggerUiComponent;
+import com.github.bubinimara.rates.di.component.UiComponent;
+import com.github.bubinimara.rates.di.module.ApplicationModule;
+
 /**
  * Created by davide.
  */
 public class RatesApp extends Application {
+
+    private UiComponent uiComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ApplicationComponent applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(getAplicationModule())
+                .build();
+
+        uiComponent = DaggerUiComponent.builder()
+                .applicationComponent(applicationComponent)
+                .build();
+    }
+
+    protected ApplicationModule getAplicationModule() {
+        return new ApplicationModule(this);
+    }
+
+    public UiComponent getUiComponent() {
+        return uiComponent;
+    }
 }
